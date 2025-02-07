cfa68ab93954f7af194a0a42006fb083da1de25f
Jay <atmajaya.dw@gmail.com>
2025-02-07 20:29:40 +0700

    Create Navbar

---
+++ src/assets/img/img.js
@@ -0,0 +1,26 @@
+import BG1 from "./BG1.jpg";
+import BG2 from "./BG2.jpg";
+import BG3 from "./BG3.jpg";
+import BG4 from "./BG4.jpg";
+import BG5 from "./BG5.jpg";
+import Jumbo_portfolio from "./Jumbo_portfolio.jpg";
+import Jumbo_contact from "./Jumbo_contact.jpg";
+import Jumbo_about from "./Jumbo_about.jpg";
+import Thumbail1 from "./thumbnail1.jpg";
+import Thumbail2 from "./thumbnail2.jpg";
+import Thumbail3 from "./thumbnail3.jpg";
+import Logo from "./empathielogo.png";
+export {
+  BG1,
+  BG2,
+  BG3,
+  BG4,
+  BG5,
+  Jumbo_about,
+  Jumbo_portfolio,
+  Jumbo_contact,
+  Thumbail1,
+  Thumbail2,
+  Thumbail3,
+  Logo,
+};
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,24 +1,61 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, NavLink } from "react-router-dom";
+import { Logo } from "../assets/icons/icons.js";
 
 const Navbar = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/portfolio">Portfolio</Link>
-          </li>
-          <li>
-            <Link to="/about-us">About Us</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+      <section id="navbar">
+        <div className="container">
+          <div className="row navbar">
+            <div className="logo">
+                <Link to={"/"}>
+                  <img src={Logo} alt="logo"></img>
+                </Link>
+            </div>
+
+            <div className="links">
+              <ul>
+                <li>
+                  <NavLink
+                      className="link home" rel="noopenner noreferrer"
+                      to={{pathname: "/"}}
+                  >
+                      {" "}
+                      <span>Home</span>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                        className="link portfolio" rel="noopenner noreferrer"
+                        to={{pathname: "/portfolio"}}
+                    >
+                        {" "}
+                        <span>Portfolio</span>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                        className="link about" rel="noopenner noreferrer"
+                        to={{pathname: "/about-us"}}
+                    >
+                        {" "}
+                        <span>About</span>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                        className="link contact" rel="noopenner noreferrer"
+                        to={{pathname: "/contact"}}
+                    >
+                        {" "}
+                        <span>Contact</span>
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
 
       <Outlet />
     </>
